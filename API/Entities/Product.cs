2a04e5672230041a4d7836d9c6896ea163d4a39a
aeross <andrewpratama22@gmail.com>
2024-04-19 18:41:10 +1000

    initial template

---
+++ API/Entities/Product.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public long Price { get; set; }  // shift the decimal places 2 times to the left, i.e. $100.00 is stored as 10000
+
+        public string PictureUrl { get; set; }
+        public string Type { get; set; }
+        public string Brand { get; set; }
+        public int QuantityInStock { get; set; }
+    }
+}
