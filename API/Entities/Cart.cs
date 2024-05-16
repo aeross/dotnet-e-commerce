a0b8aa4482cba34abf23186ff2c5fbeb9b5be589
aeross <andrewpratama22@gmail.com>
2024-05-16 22:35:09 +1000

    add cart entity

---
+++ API/Entities/Cart.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public string BuyerId { get; set; }
+        public List<CartItem> Items { get; set; } = [];
+
+        public void AddItem(Product product, int qty)
+        {
+            // check if product is not already in cart
+            if (Items.All(item => item.ProductId != product.Id))
+            {
+                // add product to cart
+                Items.Add(new CartItem { Product = product, Qty = qty });
+            }
+            else
+            {
+                var item = Items.FirstOrDefault(item => item.ProductId == product.Id);
+                if (item != null) item.Qty += qty;
+            }
+        }
+
+        public void RemoveItem(int productId, int qty)
+        {
+            var item = Items.FirstOrDefault(item => item.ProductId == productId);
+            if (item == null) return;
+
+            item.Qty -= qty;
+            if (item.Qty <= 0) Items.Remove(item);
+        }
+    }
+}
---
+++ API/Entities/CartItem.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    [Table("CartItems")]
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int Qty { get; set; }
+
+        // navigation properties
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int CartId { get; set; }
+        public Cart Cart { get; set; }
+    }
+}
