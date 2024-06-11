ce71d5cf7295e96ec970959a8229a9f0712a9cc3
aeross <andrewpratama22@gmail.com>
2024-06-11 13:14:53 +1000

    login and register features

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
@@ -13,6 +15,35 @@ namespace API.Controllers
         public AccountController(UserManager<User> userManager)
         {
             _userManager = userManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login([FromBody] LoginDto loginDto)
+        {
+            var user = await _userManager.FindByNameAsync(loginDto.Username);
+            if (user == null || !await _userManager.CheckPasswordAsync(user, loginDto.Password))
+                return Unauthorized();
+
+            return user;
+        }
+
+        public async Task<ActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            var user = new User { UserName = registerDto.Username, Email = registerDto.Email };
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+                return ValidationProblem();
+            }
+
+            await _userManager.AddToRoleAsync(user, "Member");
+
+            return StatusCode(201);
         }
     }
 }
